c92f6f4aaf03f6d499ef56b93c9a8b18189f9229
Mike Hommey <mh@glandium.org>
2018-04-26 20:57:19 +0900

    Run nightly cargo fmt

--- src/allocator_box.rs
+++ src/allocator_box.rs
@@ -6,8 +6,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use core::ptr;
 use allocator_api::{Alloc, Box, RawVec};
+use core::ptr;
 use {BoxExt, Zero};
 
 /// Extensions to the `allocator_api::Box` type
