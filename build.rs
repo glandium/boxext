0f148deda353ba03fb3670e99de62a0d25ca9061
Mike Hommey <mh@glandium.org>
2018-06-20 13:23:19 +0900

    Fix building with 1.28

--- build.rs
+++ build.rs
@@ -5,11 +5,13 @@ use rustc_version::{version, Version};
 fn main() {
     if version().unwrap() >= Version::parse("1.27.0-beta").unwrap() {
         println!("cargo:rustc-cfg=feature=\"nonnull_cast\"");
-        println!("cargo:rustc-cfg=feature=\"global_alloc27\"");
     }
     if version().unwrap() >= Version::parse("1.28.0-alpha").unwrap() {
         println!("cargo:rustc-cfg=feature=\"global_alloc\"");
     } else {
+        if version().unwrap() >= Version::parse("1.27.0-beta").unwrap() {
+            println!("cargo:rustc-cfg=feature=\"global_alloc27\"");
+        }
         // We use pre-global-alloc unstable features for rust < 1.28.
         println!("cargo:rustc-env=RUSTC_BOOTSTRAP=1");
     }
